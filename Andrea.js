b36c6ec2ab2b7b2494313ac331df894463368bd9
Emiliano Zuchovicki <emilianozuchovicki@hotmail.com>
2021-02-13 00:58:06 -0600

    Final Changes

---
+++ Andrea.js
@@ -0,0 +1,8 @@
+window.onload = init 
+function init(){
+    document.querySelector('#BotonInicio').addEventListener('click',function(){
+        window.location.href = "Main.html"
+    })
+}
+
+
---
+++ Main.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var options;
+    var elems = document.querySelectorAll('.carousel');
+    var instances = M.Carousel.init(elems, options);
+  });
+
+
+  document.addEventListener('DOMContentLoaded', function() {
+      var options;
+    var elems = document.querySelectorAll('.modal');
+    var instances = M.Modal.init(elems, options);
+  });
